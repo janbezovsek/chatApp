6aded2c43832891e3f643814acf14dae36753175
alfa1omega <bezovsekjan@gmail.com>
2024-09-03 12:10:51 +0200

    Working Login and Register components

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react'
+import axios from 'axios'
 import { Button } from '@chakra-ui/button'
 import {VStack} from "@chakra-ui/layout"
 import { FormControl, FormLabel } from "@chakra-ui/form-control"
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input"
+import { useToast } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 
 
 
 const Login = () => {
 
-    
+    //state for email
     const [email, setEmail] = useState()
+
+    //state for password
     const [password, setPassword] = useState()
+
+
     const [show, setShow] = useState(false)
+
+    const [loading, setLoading] = useState(false)
+
+    //for form functionality when typed in data is correct or false
+    const toast = useToast()
+
+    const history = useNavigate()
 
 
 
@@ -20,7 +34,67 @@ const Login = () => {
         setShow(!show)
     }
 
-    const submitHandler = () => {
+    const submitHandler = async () => {
+
+        
+        setLoading(true)
+
+        if( !email || !password ) {
+            toast({
+                title: 'Please fill all of the fields.',
+                description: "Warning",
+                status: 'warning',
+                duration: 6000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            setLoading(false)
+            return
+        }
+
+
+        
+        //sending our data to designated API
+        try {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
+
+
+            const { data } = await axios.post(
+                "http://localhost:5000/api/user/login", 
+                { email, password },
+                config
+            )
+
+            toast({
+                title: 'Login successful.',
+                description: "You are now loged in",
+                status: 'success',
+                duration: 6000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            
+            localStorage.setItem("userInfo", JSON.stringify(data))
+            setLoading(false)
+            history.push('/chats')
+
+        } catch (error) {
+            
+            toast({
+                title: 'Error occured.',
+                description: error.response.data.message,
+                status: 'error',
+                duration: 6000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            setLoading(false)
+        }
+
 
     }
 
@@ -34,7 +108,7 @@ const Login = () => {
             <FormLabel>
                 Email
             </FormLabel>
-            <Input placeholder="Enter your email" onChange={(e)=>setEmail(e.target.value)}></Input>
+            <Input placeholder="Enter your email" value={email} onChange={(e)=>setEmail(e.target.value)}></Input>
         </FormControl>
 
         <FormControl id="password" isRequired>
@@ -42,7 +116,7 @@ const Login = () => {
                 Password
             </FormLabel>
             <InputGroup>
-            <Input type={show ? "text" : "password"} placeholder="Enter your password" onChange={(e)=>setPassword(e.target.value)}></Input>
+            <Input type={show ? "text" : "password"} placeholder="Enter your password" value={password} onChange={(e)=>setPassword(e.target.value)}></Input>
             <InputRightElement width="4.5rem">
                 <Button h="1.75rem" size="sm" onClick={handleClick}>
                     { show ? "Hide" : "Show" }
@@ -56,6 +130,7 @@ const Login = () => {
         width="100%"
         style={{marginTop: 15}}
         onClick={submitHandler}
+        isLoading={loading}
         >
             Login
         </Button>
--- client/src/components/Authentication/Register.jsx
+++ client/src/components/Authentication/Register.jsx
@@ -1,17 +1,38 @@
 import { useState } from 'react'
+import axios from 'axios'
 import { Button } from '@chakra-ui/button'
 import {VStack} from "@chakra-ui/layout"
 import { FormControl, FormLabel } from "@chakra-ui/form-control"
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input"
+import { useToast } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Register = () => {
-
+    
+    //state for user name
     const [name, setName] = useState()
+
+    //state for user email
     const [email, setEmail] = useState()
+
+    //state for confirmed password
     const [confirmpassword, setConfirmpassword] = useState()
+
+    //state for password
     const [password, setPassword] = useState()
-    const [pic, setPic] = useState()
+
+
     const [show, setShow] = useState(false)
+
+
+    const [loading, setLoading] = useState(false)
+
+    //for form functionality when typed in data is correct or false
+    const toast = useToast()
+
+
+    const history = useNavigate()
 
 
 
@@ -19,11 +40,79 @@ const Register = () => {
         setShow(!show)
     }
 
-    const postDetails = (pics) => {
 
-    }
+    const submitHandler = async () => {
 
-    const submitHandler = () => {
+        setLoading(true)
+
+        if(!name || !email || !password || !confirmpassword) {
+            toast({
+                title: 'Please fill all of the fields.',
+                description: "Warning",
+                status: 'warning',
+                duration: 6000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            setLoading(false)
+            return
+        }
+
+        if(password !== confirmpassword) {
+            toast({
+                title: 'Passwords do not match.',
+                description: "Passwords do not match",
+                status: 'warning',
+                duration: 6000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            return
+        }
+
+        
+        //sending our data to designated API
+        try {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
+
+
+            const { data } = await axios.post(
+                "http://localhost:5000/api/user", 
+                { name, email, password },
+                config
+            )
+
+            toast({
+                title: 'Registration successful.',
+                description: "You are now registered",
+                status: 'success',
+                duration: 6000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            
+            localStorage.setItem("userInfo", JSON.stringify(data))
+            setLoading(false)
+            history.push('/chats')
+
+        } catch (error) {
+            
+            toast({
+                title: 'Error occured.',
+                description: error.response.data.message,
+                status: 'error',
+                duration: 6000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            setLoading(false)
+            
+        }
+
 
     }
 
@@ -73,17 +162,13 @@ const Register = () => {
             </InputGroup>  
         </FormControl>
 
-        <FormControl id="pic">
-            <FormLabel>Upload your picture
-                <Input type="file" p={1.5} accept="image/*" onChange={(e) => postDetails(e.target.files[0])}/>
-            </FormLabel>
-        </FormControl>
 
         <Button
         colorScheme='blue'
         width="100%"
         style={{marginTop: 15}}
         onClick={submitHandler}
+        isLoading={loading}
         >
             Sign Up
         </Button>
